57d5dd9aa03a8567c729cdfe015902dcbd5ddd6a
kakkokari-gtyih <daisho7308+f@gmail.com>
2024-08-01 22:42:11 +0900

    add missing properties

--- packages/backend/src/core/entities/UserEntityService.ts
+++ packages/backend/src/core/entities/UserEntityService.ts
@@ -635,6 +635,8 @@ export class UserEntityService implements OnModuleInit {
 				isRenoteMuted: relation.isRenoteMuted,
 				notify: relation.following?.notify ?? 'none',
 				withReplies: relation.following?.withReplies ?? false,
+				stripeCustomerId: profile?.stripeCustomerId,
+				stripeSubscriptionId: user.stripeSubscriptionId,
 			} : {}),
 		} as Promiseable<Packed<S>>;
 
