c2dbe44c26c4bb1b736f50acb84a145753e1bad4
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-10-07 10:45:20 +0900

    はなモードアイコンを追加

--- packages/frontend-embed/src/components/EmNoteHeader.vue
+++ packages/frontend-embed/src/components/EmNoteHeader.vue
@@ -17,6 +17,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 		<EmA :to="notePage(note)">
 			<EmTime :time="note.createdAt" colored/>
 		</EmA>
+		<span v-if="note.visibility === 'public' && note.channel == null && note.isNoteInHanaMode === true" style="margin-left: 0.5em;" :title="`${i18n.ts._visibility[note.visibility]} (${i18n.ts._hana.hanaMode})`"><i class="ti ti-hanamisskey-hanamode"></i></span>
 		<span v-if="note.visibility !== 'public'" style="margin-left: 0.5em;">
 			<i v-if="note.visibility === 'home'" class="ti ti-home"></i>
 			<i v-else-if="note.visibility === 'followers'" class="ti ti-lock"></i>
@@ -33,6 +34,7 @@ import { } from 'vue';
 import * as Misskey from 'misskey-js';
 import { notePage } from '@/utils.js';
 import { userPage } from '@/utils.js';
+import { i18n } from '@/i18n.js';
 import EmA from '@/components/EmA.vue';
 import EmUserName from '@/components/EmUserName.vue';
 import EmAcct from '@/components/EmAcct.vue';
