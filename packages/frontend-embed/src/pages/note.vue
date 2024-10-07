e94be347a2ae56e4ad0555ea90b6df7c16d09b76
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-10-07 10:45:29 +0900

    CTA追加

--- packages/frontend-embed/src/pages/note.vue
+++ packages/frontend-embed/src/pages/note.vue
@@ -4,8 +4,13 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<div :class="$style.noteEmbedRoot">
-	<EmNoteDetailed v-if="note && !prohibited" :note="note"/>
+<div :class="[$style.noteEmbedRoot, { [$style.rounded]: embedParams.rounded }]">
+	<EmNoteDetailed v-if="note && !prohibited" :note="note" :class="$style.note"/>
+	<a v-if="note && !prohibited" :href="`${url}/notes/${noteId}`" target="_blank" :class="$style.cta">
+		<img :class="$style.particles" src="https://static-assets.misskey.flowers/misc/bg-particles/left_v1a.svg"/>
+		<div :class="$style.text">{{ i18n.ts._hana._welcome._cta.title }}</div>
+		<div :class="$style.link">{{ i18n.ts.learnMore }}</div>
+	</a>
 	<XNotFound v-else/>
 </div>
 </template>
@@ -18,12 +23,17 @@ import XNotFound from '@/pages/not-found.vue';
 import { DI } from '@/di.js';
 import { misskeyApi } from '@/misskey-api.js';
 import { assertServerContext } from '@/server-context';
+import { i18n } from '@/i18n.js';
+import { url } from '@@/js/config.js';
+import { defaultEmbedParams } from '@@/js/embed-page.js';
 
 const props = defineProps<{
 	noteId: string;
 }>();
 
 const serverContext = inject(DI.serverContext)!;
+
+const embedParams = inject(DI.embedParams, defaultEmbedParams);
 
 const note = ref<Misskey.entities.Note | null>(null);
 
@@ -47,6 +57,73 @@ if (note.value?.url != null || note.value?.uri != null) {
 
 <style lang="scss" module>
 .noteEmbedRoot {
+	background-image: linear-gradient(90deg, var(--buttonGradateA), var(--buttonGradateB));
+}
+
+.noteEmbedRoot.rounded {
+	.note {
+		border-radius: 0 0 var(--radius) var(--radius);
+	}
+}
+
+.note {
 	background-color: var(--panel);
+	box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.cta {
+	height: calc(100% + var(--radius));
+	padding: var(--margin) calc(var(--margin) * 1.5);
+	color: var(--fgOnAccent);
+	font-weight: 700;
+	display: flex;
+	position: relative;
+	align-items: center;
+	overflow: hidden;
+
+	.particles {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		transform: rotateX(180deg);
+		width: 80%;
+		opacity: 0.75;
+		pointer-events: none;
+    -webkit-user-select: none;
+    user-select: none;
+    -webkit-user-drag: none;
+	}
+
+	.text, .link {
+		position: relative;
+	}
+
+	.text {
+		font-size: 1.15em;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.link {
+		margin-left: auto;
+		flex-shrink: 0;
+		display: block;
+		border-radius: 999px;
+		padding: calc(var(--margin) / 4) var(--margin);
+		background-color: transparent;
+		color: var(--fgOnAccent);
+		border: solid 1px var(--fgOnAccent);
+		transition: color 0.2s, background-color 0.2s;
+	}
+
+	&:hover {
+		text-decoration: none;
+
+		.link {
+			background-color: var(--fgOnAccent);
+			color: var(--accent);
+		}
+	}
 }
 </style>
