d137a2ac8c058bb11b167f558ec96d5ddcd46d41
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2025-02-01 14:56:36 +0900

    fix: GTLの取得件数制限が外れていた問題を修正

--- packages/backend/src/server/api/endpoints/notes/global-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/global-timeline.ts
@@ -111,7 +111,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				}));
 			}
 
-			const notes = (await query.getMany()).filter(note => {
+			const notes = (await query.limit(ps.limit).getMany()).filter(note => {
 				if (me && isUserRelated(note, userIdsWhoBlockingMe)) return false;
 				if (me && isUserRelated(note, userIdsWhoMeMuting)) return false;
 				if (me && isInstanceMuted(note, userMutedInstances)) return false;
