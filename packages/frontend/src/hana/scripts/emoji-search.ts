e64b28ddafa82dee9a3c146c1459391f6dd13f2e
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-11 22:10:30 +0900

    fix

--- packages/frontend/src/hana/scripts/emoji-search.ts
+++ packages/frontend/src/hana/scripts/emoji-search.ts
@@ -1,13 +1,12 @@
 import * as Misskey from 'misskey-js';
 import EmojiSearch from '@/workers/emoji-search.js?worker';
 import { get, set, del } from '@/utility/idb-proxy.js';
-import { hanaStore } from '@/hana/store.js';
 import type { SearchIndex } from '@hanamisskey/browser-search';
 
 let emojiSearchWorker: Worker | null = null;
 let hasInitialized = false;
 
-if (import.meta.env.MODE !== 'test' && hanaStore.s.enableWasmEmojiSearch) {
+if (import.meta.env.MODE !== 'test') {
 	emojiSearchWorker = new EmojiSearch();
 }
 
