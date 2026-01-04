655b552c48c7a8f8d822da5aeec7908248e8f2b6
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-01-04 14:04:59 +0900

    refactor: remove unused imports

--- packages/frontend/src/components/MkTutorial.MinorSettings.vue
+++ packages/frontend/src/components/MkTutorial.MinorSettings.vue
@@ -16,7 +16,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { i18n } from '@/i18n.js';
 import { store } from '@/store.js';
 import { deepMerge } from '@/utility/merge.js';
