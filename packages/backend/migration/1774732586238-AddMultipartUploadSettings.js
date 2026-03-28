a2fbf48a51b5bb4076a71e473852a2a0434b9563
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2026-03-29 06:17:08 +0900

    fix: マイグレーションをTypeORM migration:generateで再生成

---
+++ packages/backend/migration/1774732586238-AddMultipartUploadSettings.js
@@ -0,0 +1,44 @@
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ * @typedef {import('typeorm').QueryRunner} QueryRunner
+ */
+
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
+module.exports = class AddMultipartUploadSettings1774732586238 {
+    name = 'AddMultipartUploadSettings1774732586238'
+
+    /**
+     * @param {QueryRunner} queryRunner
+     */
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "meta" ADD "useMultipartUpload" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingBucket" character varying(1024)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingEndpoint" character varying(1024)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingRegion" character varying(1024)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingAccessKey" character varying(1024)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingSecretKey" character varying(1024)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingPort" integer`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingUseSSL" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingUseProxy" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "objectStorageStagingS3ForcePathStyle" boolean NOT NULL DEFAULT true`);
+    }
+
+    /**
+     * @param {QueryRunner} queryRunner
+     */
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingS3ForcePathStyle"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingUseProxy"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingUseSSL"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingPort"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingSecretKey"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingAccessKey"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingRegion"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingEndpoint"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "objectStorageStagingBucket"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "useMultipartUpload"`);
+    }
+}
