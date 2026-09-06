48ee9ad8b53c9a74f88fb52718785fd89c9d9769
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-09-06 17:25:14 +0900

    Merge commit from fork
    * fix(backend): replace slacc ZipReader with zipjs ZipReader
    
    * fix(backend): restrict emoji import zip to admin users
    
    * fix(backend): improve filename and emoji name validation
    
    * fix(backend): improve filename validation
    
    Co-authored-by: Acid Chicken <root@acid-chicken.com>
    
    ---------
    
    Co-authored-by: Acid Chicken <root@acid-chicken.com>

--- packages/frontend/src/pages/custom-emojis-manager.vue
+++ packages/frontend/src/pages/custom-emojis-manager.vue
@@ -83,6 +83,7 @@ import * as os from '@/os.js';
 import { misskeyApi } from '@/utility/misskey-api.js';
 import { getProxiedImageUrl } from '@/utility/media-proxy.js';
 import { i18n } from '@/i18n.js';
+import { iAmAdmin } from '@/i.js';
 import { definePage } from '@/page.js';
 import { Paginator } from '@/utility/paginator.js';
 
@@ -207,7 +208,7 @@ const menu = (ev: MouseEvent) => {
 					});
 				});
 		},
-	}, {
+	}, ...(iAmAdmin ? [{
 		icon: 'ti ti-upload',
 		text: i18n.ts.import,
 		action: async () => {
@@ -230,7 +231,7 @@ const menu = (ev: MouseEvent) => {
 					});
 				});
 		},
-	}], ev.currentTarget ?? ev.target);
+	}] : [])], ev.currentTarget ?? ev.target);
 };
 
 const setCategoryBulk = async () => {
